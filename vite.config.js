c5bb74e2e2ea0c8028d59a7bcd1288825043bcde
Emmeraldio <liliundertale@gmail.com>
2025-06-11 15:59:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { path } from 'framer-motion/client'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Lab77/', // <--- ЦЕ ГОЛОВНЕ
-})
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
