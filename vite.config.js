5a5f828497b5d6fbac3c59500fb36d92a31649b1
Emmeraldio <liliundertale@gmail.com>
2025-06-11 15:52:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+  base: '/Lab77/', // <--- ЦЕ ГОЛОВНЕ
+})
